b85d2739646583f2d1b38341eb9a842d52b77742
anton <anton@mfin.co.za>
2018-12-04 08:58:42 +0200

    asd
    Signed-off-by: anton <anton@mfin.co.za>

--- tmp.sh
+++ tmp.sh
@@ -3,7 +3,7 @@ SCRIPT=$(cd $(dirname $0); /bin/pwd)
 COIN=bitcoin
 INCOMING_DIRECTORY=/data/${COIN}/incoming/
 CS_OUT_FILE=${INCOMING_DIRECTORY}cs-bitcoin-20181203-0610-CGZ3cas7.out
-COPYUTXOS="\\COPY '${COIN}'_utxo(txn_hash, txn_no, address, amount) FROM '${CS_OUT_FILE}' WITH DELIMITER ';' ON CONFLICT DO UPDATE "
+COPYUTXOS="\\COPY ${COIN}_utxo(txn_hash, txn_no, address, amount) FROM '${CS_OUT_FILE}' WITH DELIMITER ';' ON CONFLICT DO UPDATE "
 
 source "/opt/chainstate/config.cfg"
 
