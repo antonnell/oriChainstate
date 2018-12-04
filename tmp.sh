5ea806a982622b4e0469f8b6fef9d2c82fe8f600
anton <anton@mfin.co.za>
2018-12-04 08:51:53 +0200

    asd
    Signed-off-by: anton <anton@mfin.co.za>

---
+++ tmp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+SCRIPT=$(cd $(dirname $0); /bin/pwd)
+INCOMING_DIRECTORY=/data/${COIN}/incoming/
+CS_OUT_FILE=${INCOMING_DIRECTORY}cs-bitcoin-20181203-0610-CGZ3cas7.out
+COPYUTXOS="\\COPY '${COIN}'_utxo(txn_hash, txn_no, address, amount) FROM '${CS_OUT_FILE}' WITH DELIMITER ';' ON CONFLICT DO UPDATE "
+
+source "/opt/chainstate/config.cfg"
+
+echo "Importing UTXOs"
+sudo -u postgres $(export PGPASSWORD=${password}; psql --host=${host} --port=${port} --username=${username} --dbname=${database} -c "${COPYUTXOS}")
+
+exit 0
